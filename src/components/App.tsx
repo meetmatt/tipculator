0be656d9f73e27a9f33c8ad5e619a40d79f3b857
Yuri Golikov <iurii.golikov@gmail.com>
2024-12-18 18:46:54 +0100

    Extracted state, attached event handlers

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,21 @@
-import { ReactNode } from "react";
+import React, {useState} from "react"
 
-function Bill({children}: {children: ReactNode}) {
+function Bill({children, value, onChange}: { children: React.ReactNode, value: number, onChange: React.ChangeEventHandler }) {
   return <div>
     <label htmlFor="total">{children}</label>
-    <input id="total" name="total" type="number" min="1" max="100" step="0.1"/>
+    <input id="total" name="total" type="number" min="0" step="1" value={value} onChange={onChange}/>
   </div>
 }
 
-function TipSelect({id, children}: { id: string, children: ReactNode }) {
+function TipSelect({id, children, value, onChange}: {
+  id: string,
+  children: React.ReactNode,
+  value: number,
+  onChange: React.ChangeEventHandler
+}) {
   return <div>
     <label htmlFor={id}>{children}</label>
-    <select id={id} name={id}>
+    <select id={id} name={id} value={value} onChange={onChange}>
       <option value="0">Dissatisfied (0%)</option>
       <option value="5">It was okay (5%)</option>
       <option value="10">It was good (10%)</option>
@@ -19,15 +24,48 @@ function TipSelect({id, children}: { id: string, children: ReactNode }) {
   </div>
 }
 
-function Reset() {
-  return <input type="reset" value="Reset"/>
+function Summary({bill, tip}: { bill: number, tip: number }) {
+  const sum = Number(bill) + Number(tip)
+  return <h1>You pay ${sum} (${bill} + ${tip} tip)</h1>
+}
+
+function Reset({onClick}: { onClick: React.EventHandler<React.MouseEvent<HTMLInputElement>> }) {
+  return <input type="reset" value="Reset" onClick={onClick}/>
 }
 
 export default function App() {
-  return <form>
-    <Bill>How much was the bill?</Bill>
-    <TipSelect id="ownLike">How did you like the service?</TipSelect>
-    <TipSelect id="friendLike">How did your friend like the service?</TipSelect>
-    <Reset/>
+  const [bill, setBill] = useState<number>(0)
+  const [ownLike, setOwnLike] = useState<number>(0)
+  const [friendLike, setFriendLike] = useState<number>(0)
+  const tip = Number((bill * (ownLike + friendLike) / 200).toFixed(2))
+
+  function handleBillOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setBill(Number(e.target.value))
+  }
+
+  function handleOwnLikeOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setOwnLike(Number(e.target.value))
+  }
+
+  function handleFriendLikeOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFriendLike(Number(e.target.value))
+  }
+
+  function handleOnReset() {
+    setBill(0)
+    setOwnLike(0)
+    setFriendLike(0)
+  }
+
+  return <form onSubmit={(e) => e.preventDefault()}>
+    <Bill value={bill} onChange={handleBillOnChange}>How much was the bill?</Bill>
+    <TipSelect id="ownLike" value={ownLike} onChange={handleOwnLikeOnChange}>
+      How did you like the service?
+    </TipSelect>
+    <TipSelect id="friendLike" value={friendLike} onChange={handleFriendLikeOnChange}>
+      How did your friend like the service?
+    </TipSelect>
+    <Summary bill={bill} tip={tip}/>
+    <Reset onClick={handleOnReset}/>
   </form>
 }
