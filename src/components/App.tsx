6fe10d623b7dbdac3559f0cdfb4ce526a7ac8a96
Yuri Golikov <iurii.golikov@gmail.com>
2024-12-18 16:45:16 +0100

    Initialized Tip Calculator App component with simple HTML and CSS
    # Conflicts:
    #	src/components/App.tsx
    #	src/index.html
    #	src/index.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,30 @@
+
 export default function App() {
-  return <h1>Hello from React!</h1>
+  return <>
+    <form>
+      <field>
+        <label htmlFor="total">How much was the bill?</label>
+        <input id="total" name="total" type="number" min="1" max="100" step="0.1" />
+      </field>
+      <div>
+        <label htmlFor="ownLike">How did you like the service?</label>
+        <select id="ownLike" name="ownLike">
+          <option value="0">Dissatisfied (0%)</option>
+          <option value="5">It was okay (5%)</option>
+          <option value="10">It was good (10%)</option>
+          <option value="20">Absolutely amazing! (20%)</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="friendLike">How did you like the service?</label>
+        <select id="friendLike" name="friendLike">
+          <option value="0">Dissatisfied (0%)</option>
+          <option value="5">It was okay (5%)</option>
+          <option value="10">It was good (10%)</option>
+          <option value="20">Absolutely amazing! (20%)</option>
+        </select>
+      </div>
+      <input type="reset" value="Reset" />
+    </form>
+  </>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './components/App.tsx'
 import "./index.css"
+import './main.css'
 
 createRoot(window.document.getElementById('root')!).render(
   <StrictMode>
